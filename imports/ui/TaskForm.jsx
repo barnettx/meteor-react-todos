9a354c291b5facd2591d61b5b61982be69e5060a
Barnett <barnett.lee.zhi.le@gmail.com>
2021-01-02 17:00:53 +0800

    Add form to add tasks

---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { TasksCollection } from '../api/TasksCollection'
+
+export const TaskForm = () => {
+    const [text, setText] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (!text) return;
+        
+        TasksCollection.insert({
+            text: text.trim(),
+            createdAt: new Date()
+        });
+
+        setText("");
+    }
+
+    return (
+        <form className="task-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Type to add new tasks"
+                value={text}
+                onChange={(event) => setText(event.target.value)}
+            />
+            <button type="submit">Add Task</button>
+        </form>
+    )
+}
